7615c3f83792b6c4533b524d1afb02a0a588d734
Kei Hartley <ckh22@uw.edu>
2021-11-27 10:55:59 -0800

    words

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import turtle
 
 s = turtle.getscreen()
 t = turtle.Turtle()
+
+
 def main():
     # initialize
     turtle.title('Plutchiks')
@@ -21,7 +23,6 @@ def pedal(angle: int, colors, emotion: str, words):
     t.circle(-200, 90)
     t.rt(90)
     t.end_fill()
-
 
     # Sub-divide code
     # Larger Sub-division
@@ -45,7 +46,7 @@ def pedal(angle: int, colors, emotion: str, words):
     t.circle(-200, 30)
     t.end_fill()
 
-    # Labeling 
+    # Labeling
     # Emotion
     t.pu()
     t.home()
@@ -59,19 +60,38 @@ def pedal(angle: int, colors, emotion: str, words):
     t.home()
     t.pd()
 
-    # Labeling 
+    # Labeling
     # Words
+    for i in range(3):
+        t.pu()
+        t.home()
+        if emotion == 'Anticipation/Hope':
+            t.setpos(-65 + (- 80 * i), -10)
+            t.write(words[i], False, "center")
+            t.setpos(-65 + (- 80 * i), 10)
+            t.write(words[i + 1], False, "center")
+        elif emotion == 'Like':
+            t.setpos(0, 50 + (80 * i))
+            t.write(words[i], False, "center")
+            t.setpos(0, 70 + (80 * i))
+            t.write(words[i + 1], False, "center")
+        elif emotion == "Joy/Happiness":
+            t.setpos(65 + (80 * i), -10)
+            t.write(words[i], False, "center")
+            t.setpos(65 + (80 * i), 10)
+            t.write(words[i + 1], False, "center")
+        t.home()
+        t.pd()
 
 
 # These might need to be edited later if the edits in the above
 # Land the arrow in an uneven spot.
 # Settings includes angle & color for the respective petal
 settings = {
-    'Joy/Happiness': {'angle': 0, 'colors': ['#E0EC5F', '#E3EC84', '#E6ECAC'], 'words': []},
-    'Like': {'angle': 90, 'colors': ['#79EC5F', '#9FEB8E', '#BCEDB1'], 'words': []},
-    'Anticipation/Hope': {'angle': 180, 'colors': ['#5FECE9', '#89ECEA', '#B5EBEA'], 'words': []}
+    'Joy/Happiness': {'angle': 0, 'colors': ['#E0EC5F', '#E3EC84', '#E6ECAC'], 'words': ['blank', 'blank1', 'blank', 'blank1', 'blank', 'blank1']},
+    'Like': {'angle': 90, 'colors': ['#79EC5F', '#9FEB8E', '#BCEDB1'], 'words': ['blank', 'blank1', 'blank', 'blank1', 'blank', 'blank1']},
+    'Anticipation/Hope': {'angle': 180, 'colors': ['#5FECE9', '#89ECEA', '#B5EBEA'], 'words': ['blank', 'blank1', 'blank', 'blank1', 'blank', 'blank1']}
 }
-
 
 
 main()
