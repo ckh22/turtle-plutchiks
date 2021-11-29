5cd09cbe7bc1ee568e9a7707263fc049399f3228
Kei Hartley <ckh22@uw.edu>
2021-11-28 22:39:34 -0800

    style for the labels

--- main.py
+++ main.py
@@ -65,21 +65,24 @@ def pedal(angle: int, colors, emotion: str, words):
     for i in range(3):
         t.pu()
         t.home()
+        # Edit style within each of the if statements & 
+        # use the i number to change the fontsize
+        style = ("Arial", 8, "normal")
         if emotion == 'Anticipation/Hope':
             t.setpos(-65 + (- 80 * i), -10)
-            t.write(words[i], False, "center")
+            t.write(words[i], False, "center", style)
             t.setpos(-65 + (- 80 * i), 10)
-            t.write(words[i + 1], False, "center")
+            t.write(words[i + 1], False, "center", style)
         elif emotion == 'Like':
             t.setpos(0, 50 + (80 * i))
-            t.write(words[i], False, "center")
+            t.write(words[i], False, "center", style)
             t.setpos(0, 70 + (80 * i))
-            t.write(words[i + 1], False, "center")
+            t.write(words[i + 1], False, "center", style)
         elif emotion == "Joy/Happiness":
             t.setpos(65 + (80 * i), -10)
-            t.write(words[i], False, "center")
+            t.write(words[i], False, "center", style)
             t.setpos(65 + (80 * i), 10)
-            t.write(words[i + 1], False, "center")
+            t.write(words[i + 1], False, "center", style)
         t.home()
         t.pd()
 
